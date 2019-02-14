4d71e46f76cde62ae530d61f3884950f814162ae
RosalieVanHassel <46843490+RosalieVanHassel@users.noreply.github.com>
2019-02-14 12:59:01 +0100

    eerste commit

---
+++ src/test/java/com/example/Schaakspelers/SchaakspelersApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.Schaakspelers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchaakspelersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
