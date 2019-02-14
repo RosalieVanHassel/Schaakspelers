8a119359f99943c926542e7b897a6faf4df37448
RosalieVanHassel <46843490+RosalieVanHassel@users.noreply.github.com>
2019-02-14 13:16:33 +0100

    database staat klaar

---
+++ src/main/java/com/example/Schaakspelers/model/Speler.java
@@ -0,0 +1,86 @@
+package com.example.Schaakspelers.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Speler {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String voornaam;
+    private String achternaam;
+    private String tussenvoegsel;
+    private LocalDate geboortedatum;
+    private int gewonnenPartijen;
+    private int remises;
+    private int verlorenPartijen;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public LocalDate getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setGeboortedatum(LocalDate geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public int getGewonnenPartijen() {
+        return gewonnenPartijen;
+    }
+
+    public void setGewonnenPartijen(int gewonnenPartijen) {
+        this.gewonnenPartijen = gewonnenPartijen;
+    }
+
+    public int getRemises() {
+        return remises;
+    }
+
+    public void setRemises(int remises) {
+        this.remises = remises;
+    }
+
+    public int getVerlorenPartijen() {
+        return verlorenPartijen;
+    }
+
+    public void setVerlorenPartijen(int verlorenPartijen) {
+        this.verlorenPartijen = verlorenPartijen;
+    }
+}
